a6aa9769d7716067e5846ef6da9f76b3fb6c8a01
kidungardana <vincentius.ardana@gmail.com>
2023-11-30 22:21:29 +0700

    fixing ui login

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,11 @@ export default function Home() {
         <img className='w-1/2 ml-32' src='/LogoMY.png' />
       </div>
       <div className='w-1/2	flex items-center'>
-        <div className='px-6 bg-white h-1/2 rounded-lg bg-login-card border-0 flex flex-col justify-center items-center ml-8'>
-          <div className='text-xl font-semibold mb-4 mt-5'>Student Login</div>
-          <input className='mb-4 h-5' />
-          <input className='mb-4 h-5' />
-          <button className='text-sm mb-4 bg-login-button rounded-lg px-8 py-1'>Login</button>
+        <div className='2xl:px-16 xl:px-12 w-1/2 bg-white h-1/2 rounded-lg bg-login-card border-0 flex flex-col justify-center items-center ml-8'>
+          <div className='2xl:text-5xl xl:text-3xl font-semibold 2xl:mb-16 xl:mb-12 mt-5'>Student Login</div>
+          <input className='2xl:mb-12 xl:mb-8 h-10 w-full' />
+          <input className='2xl:mb-16 xl:mb-8 h-10 w-full' />
+          <button className='2xl:text-3xl xl:text-xl mb-4 bg-login-button rounded-lg 2xl:px-16 xl:px-8 2xl:py-2 xl:py-1'>Login</button>
         </div>
       </div>
     </div>
